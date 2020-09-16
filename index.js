af418df493ef4ac3545db0a329f7d8b79e4290e8
Clarissa Reeve <clarissa.reeve@gmail.com>
2020-09-16 13:56:14 -0600

    basics working, not pretty

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
+var inquirer = require("inquirer");
+
+var generator = require("./utils/generateMarkdown.js")
+
+// inquirer
+//   .prompt([
+//     {
+//       type: "input",
+//       message: "What is your user name?",
+//       name: "username"
+//     },
+
 // array of questions for user
-const questions = [
+const questions = [{ type: "input", message: 'what is the title?', name: 'title' },
+{ type: "input", message: 'question 2?', name: 'answer2' }
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
 }
 
 // function to initialize program
 function init() {
 
+    inquirer
+        .prompt(questions)
+        // console.log(generator({ title: 'whatever' }))
+        .then(function (response) {
+            console.log(generator(response))
+        })
 }
 
 // function call to initialize program
 init();
+
+// console.log('hello world')
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,8 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  console.log('sanity check', data)
   return `# ${data.title}
-
+just checking
 `;
 }
 
